c3879222588b6c6567eb7b764b32955aae2bc60c
tan0130 <1311230692@qq.com>
2018-05-30 21:16:54 +0800

    加入UserControllerTest单元测试类

--- src/main/Java/ssm/controller/Login.java
+++
@@ -1,8 +0,0 @@
-package ssm.controller;
-
-/**
- * create by tan on 2018-05-09
- * 登录控制
- **/
-public class Login {
-}
---
+++ src/main/Java/ssm/test/UserControllerTest.java
@@ -0,0 +1,43 @@
+package ssm.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ssm.controller.UserController;
+
+/**
+ * create by tan on 2018-05-03
+ * 用于测试功能
+ * */
+@RunWith(SpringJUnit4ClassRunner.class)//此处调用Spring单元测试类
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:spring/springmvc-servlet.xml"})//加载spring容器
+public class UserControllerTest {
+    // 注入UserController
+    @Autowired
+    private UserController userController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    // 测试查找所有用户信息
+    @Test
+    public void getAllUser() throws Exception{
+        ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.get("/user/getAllUser"));
+        MvcResult mvcResult = resultActions.andReturn();
+        String result = mvcResult.getResponse().getContentAsString();
+        System.out.println(".........所有用户信息..........");
+        System.out.println(result);
+    }
+}
